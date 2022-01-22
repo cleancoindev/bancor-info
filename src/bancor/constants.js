ada6e5d32ee961b13d3ed734fcac5964016a432b
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-01-22 02:11:50 -1000

    Update constants.js

--- src/bancor/constants.js
+++ src/bancor/constants.js
@@ -1,5 +1,5 @@
 // using ISO 3166 Alpha-3 code
-export const blacklistedCountryCodes = ['USA']
+export const blacklistedCountryCodes = ['']
 const joinedCountryCodes = blacklistedCountryCodes.join(', ')
 
 export const blacklistedCountryCodeMsg = `Please note that ${joinedCountryCodes} citizens, domiciliaries or users from ${joinedCountryCodes} IPs are not allowed to convert tokens using this interface.`
